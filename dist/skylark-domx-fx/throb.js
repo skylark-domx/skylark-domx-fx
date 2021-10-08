eac5b6cf1d3bc867f3785589b0350b564e926d53
hudaokeji <github@hudaokeji.com>
2021-10-09 01:17:50 +0900

    update

--- dist/skylark-domx-fx/throb.js
+++ dist/skylark-domx-fx/throb.js
@@ -5,5 +5,5 @@
  * @link www.skylarkjs.org
  * @license MIT
  */
-define(["skylark-domx-transits","./fx"],function(e,t){return t.throb=function(e,t){(t=t||{}).text,t.style;var n,r=t.time,o=t.callback,l=noder.createElement("div",{class:t.className||"throbber"}),s=function(){n&&(clearTimeout(n),n=null),l&&(noder.remove(l),l=null)};t.style&&styler.css(l,t.style);var a=t.content||'<span class="throb"></span>';return noder.html(l,a),e.appendChild(l),r&&(n=setTimeout(function(){s(),o&&o()},r)),{throbber:l,remove:s,update:function(e){e&&e.text&&l&&(textNode.nodeValue=e.text)}}}});
+define(["skylark-domx-noder","./fx"],function(r,n){return n.throb=r.throb});
 //# sourceMappingURL=sourcemaps/throb.js.map
--- dist/uncompressed/skylark-domx-fx.js
+++ dist/uncompressed/skylark-domx-fx.js
@@ -481,81 +481,11 @@ define('skylark-domx-fx/slideUp',[
     return fx.slideUp = slideUp;
 });
 define('skylark-domx-fx/throb',[
-    "skylark-domx-transits",
+    "skylark-domx-noder",
     "./fx"
-],function(transits,fx) {
+],function(noder,fx) {
     
-    /*   
-     * Replace an old node with the specified node.
-     * @param {HTMLElement} elm
-     * @param {Node} params
-     */
-    function throb(elm, params) {
-        params = params || {};
-
-        var self = this,
-            text = params.text,
-            style = params.style,
-            time = params.time,
-            callback = params.callback,
-            timer,
-
-            throbber = noder.createElement("div", {
-                "class": params.className || "throbber"
-            }),
-            //_overlay = overlay(throbber, {
-            //    "class": 'overlay fade'
-            //}),
-            remove = function() {
-                if (timer) {
-                    clearTimeout(timer);
-                    timer = null;
-                }
-                if (throbber) {
-                    noder.remove(throbber);
-                    throbber = null;
-                }
-            },
-            update = function(params) {
-                if (params && params.text && throbber) {
-                    textNode.nodeValue = params.text;
-                }
-            };
-
-        if (params.style) {
-            styler.css(throbber,params.style);
-        }
-
-        //throb = noder.createElement("div", {
-        //   "class": params.throb && params.throb.className || "throb"
-        //}),
-        //textNode = noder.createTextNode(text || ""),
- 
-        var content = params.content ||  '<span class="throb"></span>';
-
-        //throb.appendChild(textNode);
-        //throbber.appendChild(throb);
-
-        noder.html(throbber,content);
-        
-        elm.appendChild(throbber);
-
-        var end = function() {
-            remove();
-            if (callback) callback();
-        };
-        if (time) {
-            timer = setTimeout(end, time);
-        }
-
-        return {
-            throbber : throbber,
-            remove: remove,
-            update: update
-        };
-    }
-
-    return fx.throb = throb;
+    return fx.throb = noder.throb;
 });
 define('skylark-domx-fx/toggle',[
     "skylark-domx-transits",
